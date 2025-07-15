2a7b397009dd8d3b18c1fdaebe00eeeb54d093a8
Anamya01 <anamya666@gmail.com>
2025-07-15 12:17:32 +0530

    commit

--- api/news.js
+++ api/news.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   const { query = 'india', page = 1 } = req.query;
 
-  const apiKey = process.env.REACT_APP_NEWS_API_KEY;
+  const apiKey = process.env.NEWS_API_KEY;
   const endpoint = `https://newsapi.org/v2/everything?q=${query}&pageSize=5&page=${page}&apiKey=${apiKey}`;
 
   try {
